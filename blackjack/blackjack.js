36eed9d91ce81e175051243009383bdf672cc100
bdas615 <bdas@brandinstitute.com>
2023-03-28 15:57:19 +0530

    Ongoing Black jack JS projects

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -3,15 +3,20 @@ let firstCard = 2;
 let secondCard = 5;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
+let isAlive = true;
+
+let message = "";
 
 if(sum<=20){
-    console.log("Do you want to draw a new card?")
+    message = "Do you want to draw a new card?";
 } else if(sum === 21){
-    console.log("Yoohoo!!You have got blackjack");
+    message = "Yoohoo!!You have got blackjack";
     hasBlackJack = true;
 } else {
-    console.log("You're game is over!");
+    message = "You're game is over!";
+    isAlive = false;
 }
 
+console.log(message);
 
 
